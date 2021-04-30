44d2c4c819b9df62ca7e2396f7633a0664639070
Andre Willik Valenti <awvalenti@gmail.com>
2021-04-29 22:43:41 -0300

    Renaming tocai to pp; refactorings

--- pp_main.sh
+++ pp_main.sh
@@ -2,36 +2,35 @@
 # termux-setup-storage
 # pulseaudio --start
 
-tocai_player_pid=
+pp_player_pid=
 
-tocai_main()
-(
-  unset -f tocai_main
+pp_main()
+{
+  unset -f pp_main
 
   play_vlc_windows()
   {
-    local file
-    local args=()
+    local file args=()
     for file; do
-      args+=("$(cygpath -w "$file")")
+      args+=("`cygpath -w \"$file\"`")
     done
 
     '/c/Program Files/VideoLAN/VLC/vlc' \
       -I dummy --one-instance -- "${args[@]}" >/dev/null 2>&1 &
 
-    [ -z $tocai_player_pid ] && tocai_player_pid=$!
+    [ -z $pp_player_pid ] && pp_player_pid=$!
   }
 
   play_sox_linux()
   {
     stop_player
     play -q -- "$@" >/dev/null 2>&1 &
-    tocai_player_pid=$!
+    pp_player_pid=$!
   }
 
   stop_player()
   {
-    kill -- $tocai_player_pid 2>/dev/null
+    [ $pp_player_pid ] && [ -d /proc/$pp_player_pid ] && kill -- $pp_player_pid
   }
 
   local play_cmd
@@ -41,9 +40,7 @@ tocai_main()
     play_cmd=play_sox_linux
   fi
 
-  local valid_dirs=()
-
-  local dir
+  local valid_dirs=() dir
   for dir in "$@"; do
     [ -d "$dir" ] && valid_dirs+=("$dir")
   done
@@ -51,15 +48,10 @@ tocai_main()
   local dirs_with_albums="${valid_dirs[@]}"
 
   # TODO Filter
-  # local dirs_with_albums=()
-  # for dir in "$valid_dirs"; do
-  #   $(ls "$dir"/*.{mp3} )
 
   while :; do
 
-    local albums=()
-    local index=1
-    local album
+    local albums=() index=1 album
     echo '00. Stop'
     for dir in "${dirs_with_albums[@]}"; do
       for album in "$dir"/*; do
@@ -72,17 +64,17 @@ tocai_main()
     done
     echo '99. Quit'
 
-    local option
-    local files=()
+    local option files=()
     read -r -n 2 -p 'Choose album: ' option
     if ! [[ $option =~ ^[0-9][0-9]$ ]]; then
       printf " - invalid option\n\n"
     elif [ $option -eq 0 ]; then
       stop_player
-      tocai_player_pid=
+      pp_player_pid=
       printf " - stopping\n\n"
     elif [ $option -eq 99 ]; then
       stop_player
+      unset pp_player_pid
       printf " - exiting\n\n"
       return 2>/dev/null || exit
     elif [ $option -lt 0 ] || [ $option -ge $index ]; then
@@ -90,21 +82,10 @@ tocai_main()
     else
       album="${albums[${option#0}]}"
       printf " - ok, let's play $album!\n\n"
-      readarray -d $'\0' audio_files < <(find "$album" \( -name '*.mp3' -or -name '*.flac' \) -print0 | sort -nz)
-
-      # audio_files=()
-
-      # for file in "${all_files[@]}"; do
-      #   soxi "$file" > /dev/null 2> /dev/null && audio_files+=("$file")
-      # done
-
-      # # TODO Fix
-      # IFS=$'\n' sorted_files=($(sort -n <<<"${audio_files[@]}"))
-      # unset IFS
-
+      readarray -d '' audio_files < <(find "$album" \( -name '*.mp3' -or -name '*.flac' \) -print0 | sort -nz)
       $play_cmd "${audio_files[@]}"
     fi
   done
-)
+}
 
-tocai_main "$@"
+pp_main "$@"
